3d3e4dbbf52b66cb9a6d7f08101ce8b999933696
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-04-17 11:38:27 +0100

    refactor: Rename primaryRed to primary and add secondary and error colors

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryRed = Color(0xFFD32F2F);
+  static const Color primary = Color(0xFFD32F2F);
   static const Color accentOrange = Color(0xFFF4A261);
   static const Color darkGray = Color(0xFF1D3557);
   static const Color lightGray = Color(0xFFA8DADC);
   static const Color white = Color(0xFFFFFFFF);
+
+  static const Color secondary = Colors.blue;
+
+  static const Color error = Color.fromARGB(255, 245, 172, 172);
 }
