c222599406ea4941be9848e782377d8d90de2257
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-04-22 19:02:20 +0100

    feat Added utility class for image assets

---
+++ lib/utils/constants/app_images.dart
@@ -0,0 +1,8 @@
+/// Utility class for all available image asset for the app
+class AppImages {
+  /// DO NOT INITIALIZE
+  AppImages._();
+
+  // -- App Logo
+  static const String appLogo = 'assets/images/splash_page.png';
+}
