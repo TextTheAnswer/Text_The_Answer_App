e7bac183b78b7a5ee574cca1c386656f93ab1599
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-04-22 19:40:03 +0100

    fix: Change initial route from login to home

--- lib/main.dart
+++ lib/main.dart
@@ -71,7 +71,7 @@ class _MyAppState extends State<MyApp> {
             theme: AppTheme.lightTheme(),
             darkTheme: AppTheme.darkTheme(),
             themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
-            initialRoute: Routes.login,
+            initialRoute: Routes.home,
             onGenerateRoute: AppRouter.generateRoute,
             builder: (context, child) {
               return BlocListener<AuthBloc, AuthState>(
