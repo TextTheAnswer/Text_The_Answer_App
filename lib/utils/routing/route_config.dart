3360715da2f0548ce0a5b114086c8473d4f72442
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-05-10 02:44:10 +0100

    feat: Add routes for public and private lobby screens

--- lib/utils/routing/route_config.dart
+++ lib/utils/routing/route_config.dart
@@ -10,6 +10,8 @@ import 'package:text_the_answer/screens/auth/reset_password_screen.dart';
 import 'package:text_the_answer/screens/auth/splash_screen.dart';
 import 'package:text_the_answer/screens/daily_quiz_screen.dart';
 import 'package:text_the_answer/screens/game/game_mode_screen.dart';
+import 'package:text_the_answer/screens/game/lobby_screen.dart';
+import 'package:text_the_answer/screens/game/public_lobby_screen.dart';
 import 'package:text_the_answer/screens/home/new_home_screen.dart';
 import 'package:text_the_answer/screens/main_app_screen.dart';
 import 'package:text_the_answer/screens/profile/profile_creation_screen.dart';
@@ -28,10 +30,140 @@ final GlobalKey<NavigatorState> _sectionNavigatorKey =
     GlobalKey<NavigatorState>(debugLabel: 'sectionNav');
 
 final GoRouter router = GoRouter(
-  debugLogDiagnostics: true,
   navigatorKey: _rootNavigatorKey,
-  initialLocation: AppRoutePath.login,
+  debugLogDiagnostics: true,
+  initialLocation: AppRoutePath.home,
   routes: <RouteBase>[
+    // -- Splash
+    GoRoute(
+      name: AppRouteName.splash,
+      path: AppRoutePath.splash,
+      builder: (context, state) => const SplashScreen(),
+    ),
+
+    // -- Onboarding
+    GoRoute(
+      name: AppRouteName.onboarding,
+      path: AppRoutePath.onboarding,
+      builder: (context, state) => const OnboardingScreen(),
+    ),
+
+    // -- Login
+    GoRoute(
+      name: AppRouteName.login,
+      path: AppRoutePath.login,
+      builder: (context, state) => const LoginScreen(),
+    ),
+
+    // -- Signup
+    GoRoute(
+      name: AppRouteName.signup,
+      path: AppRoutePath.signup,
+      builder: (context, state) => const RegisterScreen(),
+    ),
+
+    // -- Forget Password
+    GoRoute(
+      name: AppRouteName.forgotPassword,
+      path: AppRoutePath.forgotPassword,
+      builder: (context, state) => const ForgotPasswordScreen(),
+    ),
+
+    // -- OTP Verification
+    GoRoute(
+      name: AppRouteName.otpVerification,
+      path: AppRoutePath.otpVerificationPattern,
+      builder: (context, state) {
+        final email = state.pathParameters['email'] ?? '';
+
+        return OTPVerificationScreen(email: email);
+      },
+    ),
+
+    // -- Reset Password
+    GoRoute(
+      name: AppRouteName.resetPassword,
+      path: AppRoutePath.resetPassword,
+      builder: (context, state) {
+        final email = state.uri.queryParameters['email'] ?? '';
+        final resetToken = state.uri.queryParameters['resetToken'] ?? '';
+
+        return ResetPasswordScreen(email: email, resetToken: resetToken);
+      },
+    ),
+
+    // -- Profile Create
+    GoRoute(
+      name: AppRouteName.profileCreate,
+      path: AppRoutePath.profileCreate,
+      builder: (context, state) {
+        return ProfileCreationScreen();
+      },
+    ),
+
+    // -- Private Lobby
+    GoRoute(
+      path: AppRoutePath.privateLobby,
+      name: AppRouteName.privateLobby,
+      parentNavigatorKey: _rootNavigatorKey,
+      builder: (context, state) => LobbyScreen(isPublic: false),
+    ),
+
+    // -- Public Lobby
+    GoRoute(
+      path: AppRoutePath.publicLobby,
+      name: AppRouteName.publicLobby,
+      parentNavigatorKey: _rootNavigatorKey,
+      builder: (context, state) => PublicLobbyScreen(),
+    ),
+
+    // -- Settings
+    GoRoute(
+      name: AppRouteName.settings,
+      path: AppRoutePath.settings,
+      parentNavigatorKey: _rootNavigatorKey,
+      builder: (context, state) => const SettingsScreen(),
+      routes: <RouteBase>[
+        // -- Edit Profile Screen
+        // -- TODO
+
+        // --Notification
+        GoRoute(
+          name: AppRouteName.notification,
+          path: AppRoutePath.notification,
+          builder: (context, state) => NotificationScreen(),
+        ),
+
+        // -- Music and Effects
+        GoRoute(
+          name: AppRouteName.musicEffect,
+          path: AppRoutePath.musicEffect,
+          builder: (context, state) => MusicEffectScreen(),
+        ),
+
+        // -- Security
+        GoRoute(
+          name: AppRouteName.security,
+          path: AppRoutePath.security,
+          builder: (context, state) => SecurityScreen(),
+        ),
+
+        // -- Help Center
+        GoRoute(
+          name: AppRouteName.helpCenter,
+          path: AppRoutePath.helpCenter,
+          builder: (context, state) => HelpCenterScreen(),
+        ),
+
+        // -- About
+        GoRoute(
+          name: AppRouteName.about,
+          path: AppRoutePath.about,
+          builder: (context, state) => AboutScreen(),
+        ),
+      ],
+    ),
+
     // -- Main App Screen
     StatefulShellRoute.indexedStack(
       builder: (context, state, navigationShell) {
@@ -102,118 +234,6 @@ final GoRouter router = GoRouter(
               },
             ),
           ],
-        ),
-      ],
-    ),
-
-    // -- Splash
-    GoRoute(
-      name: AppRouteName.splash,
-      path: AppRoutePath.splash,
-      builder: (context, state) => const SplashScreen(),
-    ),
-
-    // -- Onboarding
-    GoRoute(
-      name: AppRouteName.onboarding,
-      path: AppRoutePath.onboarding,
-      builder: (context, state) => const OnboardingScreen(),
-    ),
-
-    // -- Login
-    GoRoute(
-      name: AppRouteName.login,
-      path: AppRoutePath.login,
-      builder: (context, state) => const LoginScreen(),
-    ),
-
-    // -- Signup
-    GoRoute(
-      name: AppRouteName.signup,
-      path: AppRoutePath.signup,
-      builder: (context, state) => const RegisterScreen(),
-    ),
-
-    // -- Forget Password
-    GoRoute(
-      name: AppRouteName.forgotPassword,
-      path: AppRoutePath.forgotPassword,
-      builder: (context, state) => const ForgotPasswordScreen(),
-    ),
-
-    // -- OTP Verification
-    GoRoute(
-      name: AppRouteName.otpVerification,
-      path: AppRoutePath.otpVerificationPattern,
-      builder: (context, state) {
-        final email = state.pathParameters['email'] ?? '';
-
-        return OTPVerificationScreen(email: email);
-      },
-    ),
-
-    // -- Reset Password
-    GoRoute(
-      name: AppRouteName.resetPassword,
-      path: AppRoutePath.resetPassword,
-      builder: (context, state) {
-        final email = state.uri.queryParameters['email'] ?? '';
-        final resetToken = state.uri.queryParameters['resetToken'] ?? '';
-
-        return ResetPasswordScreen(email: email, resetToken: resetToken);
-      },
-    ),
-
-    GoRoute(
-      name: AppRouteName.profileCreate,
-      path: AppRoutePath.profileCreate,
-      builder: (context, state) {
-        return ProfileCreationScreen();
-      },
-    ),
-
-    // -- Settings
-    GoRoute(
-      name: AppRouteName.settings,
-      path: AppRoutePath.settings,
-      builder: (context, state) => const SettingsScreen(),
-      routes: <RouteBase>[
-        // -- Edit Profile Screen
-        // -- TODO
-
-        // --Notification
-        GoRoute(
-          name: AppRouteName.notification,
-          path: AppRoutePath.notification,
-          builder: (context, state) => NotificationScreen(),
-        ),
-
-        // -- Music and Effects
-        GoRoute(
-          name: AppRouteName.musicEffect,
-          path: AppRoutePath.musicEffect,
-          builder: (context, state) => MusicEffectScreen(),
-        ),
-
-        // -- Security
-        GoRoute(
-          name: AppRouteName.security,
-          path: AppRoutePath.security,
-          builder: (context, state) => SecurityScreen(),
-        ),
-
-        // -- Help Center
-        GoRoute(
-          name: AppRouteName.helpCenter,
-          path: AppRoutePath.helpCenter,
-          builder: (context, state) => HelpCenterScreen(),
-        ),
-
-        // -- About
-        GoRoute(
-          name: AppRouteName.about,
-          path: AppRoutePath.about,
-          builder: (context, state) => AboutScreen(),
         ),
       ],
     ),
