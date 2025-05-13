728ac6a23daf36b08cf959588817cbd267dd9d66
Daniel Olayina <oluwaferanmiidaniel@gmail.com>
2025-05-13 02:51:15 +0100

    feat: Refactor HomeScreen to use ProfileHeader widget in place of _buildProfileCard method

---
+++ lib/widgets/quiz/profile_stats.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProfileStats extends StatelessWidget {
+  const ProfileStats({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
